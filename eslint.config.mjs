9e848fbf4da7e5fe71f084190843f2b7d3c9c335
Vsevolod Zavatskiy <vsevolod.zavatskiy@BEG-NB-S0014379.local>
2026-06-12 14:55:09 +0200

    Fix Vercel build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import coreWebVitals from "eslint-config-next/core-web-vitals.js";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
